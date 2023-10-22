58157ede187045f4868bc06f74729379caf9438b
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-22 22:55:31 +0530

    Initial Commit

---
+++ Showing task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Showing task
+//
+//  Created by sudeepa pal on 22/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Showing task/Showing_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Showing_taskApp.swift
+//  Showing task
+//
+//  Created by sudeepa pal on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Showing_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
